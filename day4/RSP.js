c51f19512ef606df8d832044a6862b66af7e1c8e
Kimminam <bluemind8520@gmail.com>
2022-12-30 00:51:41 +0900

    [Feat] rock / scissors / paper addEventListenr 추가

--- day4/RSP.js
+++ day4/RSP.js
@@ -1,15 +1,23 @@
-const rock = document.getElementById('rock');
-const scissors = document.getElementById('scissor');
-const paper = document.getElementById('paper');
-const RandomRSP = Math.floor(Math.random() * 2);
-const [Rock, Scissors, Paper] = document.getElementsByTagName('button');
+const rock = document.getElementById('rock'); 
+const scissors = document.getElementById('scissors'); 
+const paper = document.getElementById('paper'); 
+const Result = document.getElementById('Result');
+rock.addEventListener('click', () => {
+  const Random = Math.floor(Math.random() * 3);
+  const Rock = Math.floor(Math.random() * 3);
+  console.log(Rock)
+  Rock > Random ? Result.innerText = `승리` : Rock == Random ? Result.innerText = `무승부` : Result.innerText = `패배`;
+})
 
-let userState = "";
-
-Rock.addEventListener('click', ()=> userState="rock");
-Scissors.addEventListener('click', ResultUser);
-Paper.addEventListener('click', ResultUser);
-
-function ResultUser() {
-  
-}
+paper.addEventListener('click', () => {
+  const Random = Math.floor(Math.random() * 3);
+  const paper = Math.floor(Math.random() * 3);
+  console.log(paper)
+  paper > Random ? Result.innerText = `승리` : paper == Random ? Result.innerText = `무승부` : Result.innerText = `패배`;
+})
+scissors.addEventListener('click', () => {
+  const Random = Math.floor(Math.random() * 3);
+  const scissors = Math.floor(Math.random() * 3);
+  console.log(scissors)
+  scissors > Random ? Result.innerText = `승리` : scissors == Random ? Result.innerText = `무승부` : Result.innerText = `패배`;
+})
