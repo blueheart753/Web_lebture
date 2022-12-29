7319e247c0abfb65226d5947c2b60b5dd66690d3
kimminam <bluemind8520@gmail.com>
2022-12-29 19:51:15 +0900

    [Feat] addEventListener 변경 및 추가

--- day4/RSP.js
+++ day4/RSP.js
@@ -3,7 +3,10 @@ const scissors = document.getElementById('scissor');
 const paper = document.getElementById('paper');
 const RandomRSP = Math.floor(Math.random() * 2);
 const [Rock, Scissors, Paper] = document.getElementsByTagName('button');
-Rock.addEventListener('click', ResultUser);
+
+let userState = "";
+
+Rock.addEventListener('click', ()=> userState="rock");
 Scissors.addEventListener('click', ResultUser);
 Paper.addEventListener('click', ResultUser);
 
