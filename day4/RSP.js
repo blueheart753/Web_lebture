342feab1879969e050fdcb20d01c58aa0ad6757d
kimminam <bluemind8520@gmail.com>
2022-12-30 16:34:48 +0900

    [Feat] code Refactory & AiPic 추가

--- day4/RSP.js
+++ day4/RSP.js
@@ -3,27 +3,66 @@ const scissors = document.getElementById('scissors');
 const paper = document.getElementById('paper'); 
 const Result = document.getElementById('Result');
 const user = document.getElementById('UserPic');
+const AiPic = document.querySelector('#AiPic');
+function RandomRCP() {
+  if(Random === 1) {
+    Random = '바위'
+  }else if(Random === 2) {
+    Random = '보'
+  }else {
+    Random = '가위'
+  }
+}
 
 rock.addEventListener('click', () => {
-  const Random = Math.floor(Math.random() * 3);
-  const Rock = Math.floor(Math.random() * 3);
+  let Random = Math.floor(Math.random() * 4);
+  const Rock = 0;
   user.innerText = `바위`;
   console.log(Rock);
-  Rock > Random ? Result.innerText = `승리` : Rock == Random ? Result.innerText = `무승부` : Result.innerText = `패배`;
+  console.log(Random)
+  if(2 < Random) {
+    Result.innerText = "패배";
+    AiPic.innerText = "보";
+  }else if(Rock === Random) {
+    Result.innerText = "무승부";
+    AiPic.innerText = "바위";
+  }else {
+    Result.innerText = "승리";
+    AiPic.innerText = "가위";
+  }
 })
 
 paper.addEventListener('click', () => {
-  const Random = Math.floor(Math.random() * 3);
-  const paper = Math.floor(Math.random() * 3);
+  let Random = Math.floor(Math.random() * 4);
+  const paper = 1;
   console.log(paper);
   user.innerText = `보`;
-  paper > Random ? Result.innerText = `승리` : paper == Random ? Result.innerText = `무승부` : Result.innerText = `패배`;
+  if(Random > 2) {
+    Result.innerText = "패배";
+    AiPic.innerText = "가위";
+  }else if(Random === 1) {
+    Result.innerText = "무승부";
+    AiPic.innerText = "보";
+  }else {
+    Result.innerText = "승리";
+    AiPic.innerText = "바위";
+  }
 })
 
 scissors.addEventListener('click', () => {
-  const Random = Math.floor(Math.random() * 3);
-  const scissors = Math.floor(Math.random() * 3);
+  let Random = Math.floor(Math.random() * 4);
+  const scissors = 2;
   console.log(scissors);
+  console.log(Random)
   user.innerText = `가위`;
-  scissors > Random ? Result.innerText = `승리` : scissors == Random ? Result.innerText = `무승부` : Result.innerText = `패배`;
+  if(Random === 1) {
+    Result.innerText = "패배";
+    AiPic.innerText = "보";
+  }else if(scissors === Random) {
+    Result.innerText = "무승부";
+    AiPic.innerText = "가위";
+  }else {
+    Result.innerText = "승리";
+    AiPic.innerText = "바위";
+  }
 })
